a7588357ba199433fafc5cb77052a739f820c0e2
NobbyDK <159523433+NobbyDK@users.noreply.github.com>
2024-02-17 01:11:21 +0700

    Strings.java

--- Strings.java
+++ Strings.java
@@ -3,21 +3,24 @@ public class Strings {
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-
+        //scan inputan dari user
         String A;
         String B;
 
         A = scan.nextLine();
         B = scan.nextLine();
-        scan.close();
+        scan.close(); //menutup scanner
         
-        System.out.println("---------------");
+        System.out.println("---------------");    //menampilkan batas antara hasil scan dan ouput
 
+        //menghitung panjang string A dan string B
         int lengthA = A.length();
         int lengthB = B.length();
 
+        //menampilkan jumlah karakter string A dan string B
         System.out.println(lengthA + lengthB);
 
+        //membandingkan string A dan String B
         if(A.compareTo(B)>0)
         {
             System.out.println("Yes");
@@ -27,19 +30,21 @@ public class Strings {
             System.out.println("No");
         }
         
+            //menggabungkan string A dan string B dengan Spasi diantaranya
             String mergedInput = A + " " + B;
 
             String[] words = mergedInput.split(" ");
 
+            // Membuat StringBuilder untuk menyusun string output
             StringBuilder output = new StringBuilder();
             for (String word : words) 
             {
             // Mengonversi huruf pertama setiap kata menjadi huruf besar
-                output.append(Character.toUpperCase(word.charAt(0)))
-                      .append(word.substring(1))
-                      .append(" ");
+                output.append(Character.toUpperCase(word.charAt(0)))    //Mengonversi karakter pertama menjadi huruf besar
+                      .append(word.substring(1))    //Menambahkan sisa kata setelah huruf pertama
+                      .append(" ");    //Menambahkan spasi setelah setiap kata
              }
-             System.out.println(output.toString().trim());
+             System.out.println(output.toString().trim());    //Menampilkan output dengan menghapus spasi tambahan di ujung string
     }
 }
 
