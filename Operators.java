2e7c7a31905e7baf070e68c65f9552b3ed975d02
NobbyDK <159523433+NobbyDK@users.noreply.github.com>
2024-02-17 01:21:31 +0700

    Operators.java

--- Operators.java
+++ Operators.java
@@ -1,9 +1,9 @@
 
 public class Operators{
         public static void main(String[] args)
-        { double x = 92.98;
-        int nx = (int) Math.round(x);
-        System.out.print(nx);
+        { 
+            double x = 92.98;               // Mendeklarasikan variabel double
+            int nx = (int) Math.round(x);    // Mengonversi nilai double ke int dengan pembulatan, jika angka dibelakang koma berada dibawah 5 akan dibulatkan kebawah begitupun sebaliknya
+            System.out.print(nx);   //menampilkan hasil konversi
         }
     }
-
