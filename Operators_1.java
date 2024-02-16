6c42f88e851b88d9968ff9ac90a20efe62ec08fc
NobbyDK <159523433+NobbyDK@users.noreply.github.com>
2024-02-17 01:26:26 +0700

    Operators_1.java

--- Operators_1.java
+++ Operators_1.java
@@ -1,15 +1,16 @@
 public class Operators_1 
 {
-    static short methodOne(long l)
+    static short methodOne(long l)      //Mendefinisikan methodOne dengan parameter long
     {
-        int i = (int) l;
-        return (short)i;
+        int i = (int) l;            //Mengonversi nilai long menjadi int dan menyimpannya di variabel i
+        return (short)i;            //Mengembalikan nilai short dari variabel i yang dihasilkan dari nilai int
     }
     public static void main(String[] args)
     {
-        double d = 10.25;
-        float f = (float) d;
-        byte b = (byte) methodOne((long) f);
-        System.out.println(b);
+        double d = 10.25;             //Mendeklarasikan variabel double d dengan nilai 10.25
+        float f = (float) d;            // Mengonversi nilai double d dan menyimpannya di variabel float f
+        byte b = (byte) methodOne((long) f);    //Memanggil methodOne dengan argumen nilai float f yang dikonversi menjadi long
+                                                //Lalu mengonversi nilai yang dikembalikan dari methodOne menjadi byte
+        System.out.println(b);      //Menampilkan nilai byte b
     }
 }
