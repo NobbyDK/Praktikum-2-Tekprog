03450d89258f0eadafe3b3317071d8681d815dde
NobbyDK <159523433+NobbyDK@users.noreply.github.com>
2024-02-17 01:29:02 +0700

    Constants2.java

--- Constants2.java
+++ Constants2.java
@@ -1,11 +1,11 @@
 public class Constants2 
 {
-    public static final double CM_PER_INCH = 2.54;
+    public static final double CM_PER_INCH = 2.54;      //Mendefinisikan konstanta CM_PER_INCH yang bernilai 2.54
         public static void main(String[] args) 
         {
-            double paperWidth = 8.5;
-            double paperHeight = 11;
-            System.out.println("Paper size in centimeters: " + paperWidth*CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);
+            double paperWidth = 8.5;        //Mendeklarasikan variabel double paperWidth dengan nilai 8.5
+            double paperHeight = 11;         //Mendeklarasikan variabel double paperHeight dengan nilai 11
+            System.out.println("Paper size in centimeters: " + paperWidth*CM_PER_INCH + " by " + paperHeight * CM_PER_INCH);        //menghitung ukuran paper kedalam cm dan menampilkannya
         }
 }
 
