d8949169cb61df8e808e20074272ef83ac0d6cf4
NobbyDK <159523433+NobbyDK@users.noreply.github.com>
2024-02-17 01:18:34 +0700

    Soal1_DataTypes.java

--- Soal1_DataTypes.java
+++ Soal1_DataTypes.java
@@ -12,20 +12,24 @@ public class Soal1_DataTypes
 {
     public static void main(String[] args) 
     {
-      Scanner scan = new Scanner(System.in);  // Create a Scanner object
+      Scanner scan = new Scanner(System.in);  //membuat objek scanner untuk inputan
 
       System.out.print("Jumlah Data = ");
-      int Jml = scan.nextInt();
-      scan.nextLine();
+      int Jml = scan.nextInt();   //Membaca jumlah data yang akan dimasukkan
+      scan.nextLine();    //Membersihkan newline character
+
+      //Mendefinisikan array untuk menyimpan data
       String Data[] = new String[Jml];
       for (int i = 0; i < Jml; i++)
       {
-        Data[i] = scan.nextLine();
+        Data[i] = scan.nextLine();    //Memasukkan data ke dalam array
       }
       System.out.println("---------------------");
       for (int i = 0; i < Jml; i++) 
       {
-        BigInteger konversiData = new BigInteger(Data[i]);
+        BigInteger konversiData = new BigInteger(Data[i]);     // Mengonversi string menjadi BigInteger
+
+         //Memeriksa apakah nilai/data yang diinput termasuk kedalam rentang/range tipe data primitif
         if (konversiData.compareTo(BigInteger.valueOf(Long.MIN_VALUE)) < 0
             || konversiData.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) > 0 )
             {
@@ -36,6 +40,7 @@ public class Soal1_DataTypes
               System.out.println(Data[i] + " Can be fitted in: ");
             }
 
+            //Mengecek tiap tipe data untuk menampung nilai yang diinputkan user
             if (konversiData.compareTo(BigInteger.valueOf(Byte.MIN_VALUE)) >= 0
                 && konversiData.compareTo(BigInteger.valueOf(Byte.MAX_VALUE)) <= 0 )
                 {
@@ -56,7 +61,7 @@ public class Soal1_DataTypes
                 {
                   System.out.println("*Long");
                 }    
-        }scan.close();
+        }scan.close();    //menutup scanner
       }
 }  
 
